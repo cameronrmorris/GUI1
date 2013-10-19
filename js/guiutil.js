f2745dd5bbf74d161af3d7223867eac8469c0e4d
Cameron Morris <cmorris@cs.uml.edu>
2013-10-19 15:53:27 -0400

    Added assignment 5 to nav bar

--- js/guiutil.js
+++ js/guiutil.js
@@ -33,6 +33,7 @@ function showNavigation() {
     document.write('<li><a href="index.html">Assignment #2: Creating Your First Web Page</a></li>');
     document.write('<li><a href="index.html">Assignment #3: Styling Your First Website with CSS</a></li>');
     document.write('<li><a href="dynamictext.html">Assignment #4: Dynamic Text</a></li>');
+ document.write('<li><a href="dynamictableform.html">Assignment #5: Creating a Dynamic Table </a></li>');
     document.write('</ul></li>');
     document.write('<li><a href="about.html">About</a></li>');
     document.write('</ul>');
