b696bff3498d2466daa301eaf5402bf32a3ef3a7
Cameron Morris <zephyrz2712@gmail.com>
2013-11-18 21:11:55 -0500

    Updated with tabs instead of spaces because tabs:D

--- js/dynamictable.js
+++ js/dynamictable.js
@@ -1,223 +1,223 @@
 /*
-  File:  ~cmorris/GUI/dynamictext.js
-  Cameron R. Morris, UMass Lowell Computer Science, cmorris@cs.uml.edu
-  Copyright (c) 2013 by Cameron R. Morris.  All rights reserved.  May be freely
-  copied or excerpted for educational purposes with credit to the author.
+	File:	 ~cmorris/GUI/dynamictext.js
+	Cameron R. Morris, UMass Lowell Computer Science, cmorris@cs.uml.edu
+	Copyright (c) 2013 by Cameron R. Morris.	All rights reserved.	May be freely
+	copied or excerpted for educational purposes with credit to the author.
 
-  Last Updated: October 19, 2013 at 1:40 PM
+	Last Updated: October 19, 2013 at 1:40 PM
 
-  Description: This file contains the functions for the dynamic table 
-               assignment.
+	Description: This file contains the functions for the dynamic table
+	assignment.
 
-  Note: buildTable() depends on formutilities.js which is Jesse's work.
+	Note: buildTable() depends on formutilities.js which is Jesse's work.
 */
 
 
 // Form functions //
 
 /* This function will sanitize the user input
-   Source for space removal: http://goo.gl/YxuVRn */
+	 Source for space removal: http://goo.gl/YxuVRn */
 function sanitizeNumber( number ) {
 
-    var str = number;
+	var str = number;
 
-    // Remove any spaces
-    str = str.split(' ').join('');
+	// Remove any spaces
+	str = str.split(' ').join('');
 
-    if( validNumber(parseInt(str)) ) {
-	return parseInt(str);
-    }
-    else
-	return str;
+	if( validNumber(parseInt(str)) ) {
+		return parseInt(str);
+	}
+	else
+		return str;
 }
 
 
 // This function checks a value if it is a valid number.
 function validNumber( number ) {
 
-    if( number == "" || isNaN(number) ) {
-	return false;
-    }
-    return true;
+	if( number == "" || isNaN(number) ) {
+		return false;
+	}
+	return true;
 
 }
 
-/* This function validates the form input and helps the user correct it if 
-   necessary */
+/* This function validates the form input and helps the user correct it if
+	 necessary */
 function validateForm() {
 
-    var frm = document.getElementById( "tableForm" );
-    
-    // Reset borders of text boxes
-    frm.n1.style.borderColor = null;
-    frm.n2.style.borderColor = null;
-    frm.n3.style.borderColor = null;
-    frm.n4.style.borderColor = null;
-    document.getElementById("errormessage").innerHTML="";
+	var frm = document.getElementById( "tableForm" );
+
+	// Reset borders of text boxes
+	frm.n1.style.borderColor = null;
+	frm.n2.style.borderColor = null;
+	frm.n3.style.borderColor = null;
+	frm.n4.style.borderColor = null;
+	document.getElementById("errormessage").innerHTML="";
 
 
-    // Sanitize the user input and update it on the page
-    frm.n1.value = sanitizeNumber(frm.n1.value);
-    frm.n2.value = sanitizeNumber(frm.n2.value);
-    frm.n3.value = sanitizeNumber(frm.n3.value);
-    frm.n4.value = sanitizeNumber(frm.n4.value);
+	// Sanitize the user input and update it on the page
+	frm.n1.value = sanitizeNumber(frm.n1.value);
+	frm.n2.value = sanitizeNumber(frm.n2.value);
+	frm.n3.value = sanitizeNumber(frm.n3.value);
+	frm.n4.value = sanitizeNumber(frm.n4.value);
 
-    // Check each number box for a valid number
-    if( !validNumber( frm.n1.value ) ) {
+	// Check each number box for a valid number
+	if( !validNumber( frm.n1.value ) ) {
 
-	// Alert user
-	frm.n1.style.borderColor = "red";
-	frm.n1.focus();
-	document.getElementById("errormessage").innerHTML= 
-	    "Number 1 is not valid.";
-	// Cancel submit
-	return false;
+		// Alert user
+		frm.n1.style.borderColor = "red";
+		frm.n1.focus();
+		document.getElementById("errormessage").innerHTML=
+			"Number 1 is not valid.";
+		// Cancel submit
+		return false;
 
-    }
-    if( !validNumber( frm.n2.value ) ) {
+	}
+	if( !validNumber( frm.n2.value ) ) {
 
-        // Alert user
-        frm.n2.style.borderColor = "red";
-        frm.n2.focus();
-	document.getElementById("errormessage").innerHTML= 
-	    "Number 2 is not valid.";
+		// Alert user
+		frm.n2.style.borderColor = "red";
+		frm.n2.focus();
+		document.getElementById("errormessage").innerHTML=
+			"Number 2 is not valid.";
 
-        // Cancel submit
-        return false;
+		// Cancel submit
+		return false;
 
-    }
-    
-    // Multiplier beginning is greater than end
-    if( parseInt(frm.n1.value) > parseInt(frm.n2.value) ) {
-	
-	// Alert user
-        frm.n1.style.borderColor = "red";
-        frm.n1.focus();
-	frm.n2.style.borderColor = "red";
-        frm.n2.focus();
-        document.getElementById("errormessage").innerHTML=
-            "Error: Multiplier begin is greater than the end.";
-	
-        // Cancel submit
-        return false;
-	
-	
-    }
+	}
 
-    if( !validNumber( frm.n3.value ) ) {
+	// Multiplier beginning is greater than end
+	if( parseInt(frm.n1.value) > parseInt(frm.n2.value) ) {
 
-        // Alert user
-        frm.n3.style.borderColor = "red";
-        frm.n3.focus();
-	document.getElementById("errormessage").innerHTML= 
-	    "Number 3 is not valid.";
+		// Alert user
+		frm.n1.style.borderColor = "red";
+		frm.n1.focus();
+		frm.n2.style.borderColor = "red";
+		frm.n2.focus();
+		document.getElementById("errormessage").innerHTML=
+			"Error: Multiplier begin is greater than the end.";
 
-        // Cancel submit
-        return false;
-
-    }
-    if( !validNumber( frm.n4.value ) ) {
-
-        // Alert user
-        frm.n4.style.borderColor = "red";
-        frm.n4.focus();
-	document.getElementById("errormessage").innerHTML= 
-	    "Number 4 is not valid.";
-	
-        // Cancel submit
-        return false;
-
-    }    
+		// Cancel submit
+		return false;
 
 
-    // Multiplicand beginning is greater than end
-    if( parseInt(frm.n3.value) > parseInt(frm.n4.value) ) {
+	}
 
-        // Alert user
-	frm.n3.style.borderColor = "red";
-        frm.n3.focus();
-        frm.n4.style.borderColor = "red";
-        frm.n4.focus();
-        document.getElementById("errormessage").innerHTML=
-            "Error: Multiplicand begin is greater than the end.";
+	if( !validNumber( frm.n3.value ) ) {
 
-        // Cancel submit
-        return false;
+		// Alert user
+		frm.n3.style.borderColor = "red";
+		frm.n3.focus();
+		document.getElementById("errormessage").innerHTML=
+			"Number 3 is not valid.";
+
+		// Cancel submit
+		return false;
+
+	}
+	if( !validNumber( frm.n4.value ) ) {
+
+		// Alert user
+		frm.n4.style.borderColor = "red";
+		frm.n4.focus();
+		document.getElementById("errormessage").innerHTML=
+			"Number 4 is not valid.";
+
+		// Cancel submit
+		return false;
+
+	}
 
 
-    }
+	// Multiplicand beginning is greater than end
+	if( parseInt(frm.n3.value) > parseInt(frm.n4.value) ) {
+
+		// Alert user
+		frm.n3.style.borderColor = "red";
+		frm.n3.focus();
+		frm.n4.style.borderColor = "red";
+		frm.n4.focus();
+		document.getElementById("errormessage").innerHTML=
+			"Error: Multiplicand begin is greater than the end.";
+
+		// Cancel submit
+		return false;
 
 
-    // Valid input
-    return true;
+	}
+
+
+	// Valid input
+	return true;
 }
 
 
 function buildTable() {
 
-    // Get the values passed from the form
-    var numbers = new Array();
-    // Horizontal start
-    numbers[0] = parseInt(getParameter("n1"));
-    // Horizontal end
-    numbers[1] = parseInt(getParameter("n2"));
-    // Vertical start
-    numbers[2] = parseInt(getParameter("n3"));
-    // Vertical end
-    numbers[3] = parseInt(getParameter("n4"));
+	// Get the values passed from the form
+	var numbers = new Array();
+	// Horizontal start
+	numbers[0] = parseInt(getParameter("n1"));
+	// Horizontal end
+	numbers[1] = parseInt(getParameter("n2"));
+	// Vertical start
+	numbers[2] = parseInt(getParameter("n3"));
+	// Vertical end
+	numbers[3] = parseInt(getParameter("n4"));
 
-    // Holds the resulting HTML table
-    var strResult = "";
+	// Holds the resulting HTML table
+	var strResult = "";
 
-    // Title
-    strResult += '<h3>Multiplication Table of<br>Multipliers: ';
-    strResult += numbers[0] + " to ";
-    strResult += numbers[1] + ". Multiplicands: ";
-    strResult += numbers[2] + " to ";
-    strResult += numbers[3] + ".";
-    strResult += '</h3>'
+	// Title
+	strResult += '<h3>Multiplication Table of<br>Multipliers: ';
+	strResult += numbers[0] + " to ";
+	strResult += numbers[1] + ". Multiplicands: ";
+	strResult += numbers[2] + " to ";
+	strResult += numbers[3] + ".";
+	strResult += '</h3>'
 
-    // Build the table
-    strResult += '<table class="dynamictable">';
+	// Build the table
+	strResult += '<table class="dynamictable">';
 
-    // Generate top columns
-    strResult += '<tr><td></td>';
-    for( var i= numbers[0] ; i <= numbers[1]; i++ ) {
-	
-	strResult += '<td>' + i + '</td>';
-    
-    }
-    strResult += '</tr>';
-    // Generate rows headers and values
-    for( var i = numbers[2]; i <= numbers[3]; i++ ) {
+	// Generate top columns
+	strResult += '<tr><td></td>';
+	for( var i= numbers[0] ; i <= numbers[1]; i++ ) {
 
-	strResult += '<tr>';
-	strResult += '<td>' + i + '</td>';
-	for( var j = numbers[0] ; j <= numbers[1]; j++ ) {
-
-	    strResult += '<td>';
-	    var result =  i * j;
-	    // Decimal number.
-	    if( result != Math.floor( result ) ) {
-		strResult += result.toFixed(2);
-	    }
-	    // Does not contain decimal.
-	    else {
-		strResult += result;
-	    }
-	    strResult += '</td>';
+		strResult += '<td>' + i + '</td>';
 
 	}
 	strResult += '</tr>';
-    }
-    strResult += '</table>';
+	// Generate rows headers and values
+	for( var i = numbers[2]; i <= numbers[3]; i++ ) {
 
-    // Place the table on the page
-    document.getElementById("content").innerHTML=strResult;
-    
-    // Need scrolling?
-    
-    if( numbers[1]-numbers[0] > 15 ) {
-	document.getElementById("content").style.overflow = "scroll";
-    }
+		strResult += '<tr>';
+		strResult += '<td>' + i + '</td>';
+		for( var j = numbers[0] ; j <= numbers[1]; j++ ) {
+
+			strResult += '<td>';
+			var result =	i * j;
+			// Decimal number.
+			if( result != Math.floor( result ) ) {
+				strResult += result.toFixed(2);
+			}
+			// Does not contain decimal.
+			else {
+				strResult += result;
+			}
+			strResult += '</td>';
+
+		}
+		strResult += '</tr>';
+	}
+	strResult += '</table>';
+
+	// Place the table on the page
+	document.getElementById("content").innerHTML=strResult;
+
+	// Need scrolling?
+
+	if( numbers[1]-numbers[0] > 15 ) {
+		document.getElementById("content").style.overflow = "scroll";
+	}
 }
--- js/guiutil.js
+++ js/guiutil.js
@@ -1,12 +1,12 @@
 /*
-  File:  ~cmorris/GUI/guiutil.js
-  Cameron R. Morris, UMass Lowell Computer Science, cmorris@cs.uml.edu
-  Copyright (c) 2013 by Cameron R. Morris.  All rights reserved.  May be freely
-  copied or excerpted for educational purposes with credit to the author.
+	File:  ~cmorris/GUI/guiutil.js
+	Cameron R. Morris, UMass Lowell Computer Science, cmorris@cs.uml.edu
+	Copyright (c) 2013 by Cameron R. Morris.  All rights reserved.  May be freely
+	copied or excerpted for educational purposes with credit to the author.
 
-  Last Updated: September 11, 2013 at 12:40 PM
+	Last Updated: September 11, 2013 at 12:40 PM
 
-  Description: This file contains utility functions to simplify scripts and repeated elements
+	Description: This file contains utility functions to simplify scripts and repeated elements
 */
 
 
@@ -14,31 +14,31 @@
 
 function showTitle() {
 
-    document.write('<h2>UMass Lowell: 91.61 GUI Programming I</h2>');
-    document.write('<h3>Cameron Morris - Fall 2013</h1>');
-    document.write('<h5>cmorris@cs.uml.edu');
+	document.write('<h2>UMass Lowell: 91.61 GUI Programming I</h2>');
+	document.write('<h3>Cameron Morris - Fall 2013</h1>');
+	document.write('<h5>cmorris@cs.uml.edu');
 
 }
 
 // This function writes the navigation bar to the page.
 function showNavigation() {
-    
-    document.write('<nav>');
-    document.write('<ul>');
-    document.write('<li><a href="index.html">Home</a></li>');
-    // All assignment links.
-    document.write('<li><a href="assignments.html">Assignments</a>');
-    document.write('<ul>');
-    document.write('<li><a href="index.html">Assignment #1: Installing XAMMP</a></li>');
-    document.write('<li><a href="index.html">Assignment #2: Creating Your First Web Page</a></li>');
-    document.write('<li><a href="index.html">Assignment #3: Styling Your First Website with CSS</a></li>');
-    document.write('<li><a href="dynamictext.html">Assignment #4: Dynamic Text</a></li>');
- document.write('<li><a href="dynamictableform.html">Assignment #5: Creating a Dynamic Table </a></li>');
-     document.write('<li><a href="dynamictableformjQuery.html">Assignment #6: Using the jQuery Validation Plugin with Your Dynamic Table</a></li>');
-     document.write('<li><a href="grapher.html">Assignment #7: Creating a Graphing Calculator Using the HTML Canvas</a></li>');
-    document.write('</ul></li>');
-    document.write('<li><a href="about.html">About</a></li>');
-    document.write('</ul>');
-    document.write('</nav>');
+
+	document.write('<nav>');
+	document.write('<ul>');
+	document.write('<li><a href="index.html">Home</a></li>');
+	// All assignment links.
+	document.write('<li><a href="assignments.html">Assignments</a>');
+	document.write('<ul>');
+	document.write('<li><a href="index.html">Assignment #1: Installing XAMMP</a></li>');
+	document.write('<li><a href="index.html">Assignment #2: Creating Your First Web Page</a></li>');
+	document.write('<li><a href="index.html">Assignment #3: Styling Your First Website with CSS</a></li>');
+	document.write('<li><a href="dynamictext.html">Assignment #4: Dynamic Text</a></li>');
+	document.write('<li><a href="dynamictableform.html">Assignment #5: Creating a Dynamic Table </a></li>');
+	document.write('<li><a href="dynamictableformjQuery.html">Assignment #6: Using the jQuery Validation Plugin with Your Dynamic Table</a></li>');
+	document.write('<li><a href="grapher.html">Assignment #7: Creating a Graphing Calculator Using the HTML Canvas</a></li>');
+	document.write('</ul></li>');
+	document.write('<li><a href="about.html">About</a></li>');
+	document.write('</ul>');
+	document.write('</nav>');
 
 }
